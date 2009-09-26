3c964141961bd29a18911e7db8d9a4d727b80f8d
Will Larson <lethain@gmail.com>
2009-09-26 20:58:53 +0000

    Implemented in-line and referential links.

--- src/markdown.erl
+++ src/markdown.erl
@@ -59,11 +59,11 @@ markdown(<<"![", Rest/binary>>, OpenTags, Acc, Context) ->
 
 markdown(<<"[", Rest/binary>>, OpenTags, Acc, Context) ->
     case parse_link(<<"[" , Rest/binary>>, Context) of
-	{link, Rest2, Href, Text, []} ->
-	    Link = <<"<a href=\"", Href/binary, ">", Text/binary, "</a>">>,
+	{link, Rest2, Href, Text, <<"">>} ->
+	    Link = <<"<a href=\"", Href/binary, "\">", Text/binary, "</a>">>,
 	    markdown(Rest2, OpenTags, [Link | Acc], Context);
 	{link, Rest2, Href, Text, Title} ->
-	    Link = <<"<a href=\"", Href/binary, " title=\"", Title/binary, "\" >", Text/binary, "</a>">>,
+	    Link = <<"<a href=\"", Href/binary, "\" title=\"", Title/binary, "\">", Text/binary, "</a>">>,
 	    markdown(Rest2, OpenTags, [Link | Acc], Context);
 	{context, Rest2, Context2} ->
 	    markdown(Rest2, OpenTags, Acc, Context2)
@@ -93,27 +93,27 @@ markdown(<<B:1/binary, Rest/binary>>, OpenTags, Acc, Context) ->
 parse_link(Binary, Context) ->
     {Binary2, Text} = parse_link_text(Binary,[]),
     case Binary2 of
-	<<"(",Binary3/binary>> ->
+	<<"(", Binary3/binary>> ->
 	    {Binary4, Link, Title} =  parse_link_remainder(Binary3, <<")">>),
 	    {link, Binary4, Link, Text, Title};
 	<<"[",Binary3/binary>> ->
 	    {Binary4, Reference} = parse_link_text(<<"[",Binary3/binary>>, []),
-	    case proplist:get_value(Reference, Context) of
+	    case proplists:get_value(Reference, Context) of
 		{Link, Title} ->	    
 		    {link, Binary4, Link, Text, Title};
 		_ ->
 		    {syntax_error, reference_to_undeclared_link_definition}
 	    end;
 	<<":",Binary3/binary>> -> 
-	    {Binary4, Link, Title} =  parse_link_remainder(Binary3, <<")">>),
+	    {Binary4, Link, Title} =  parse_link_remainder(Binary3, <<"\n">>),
 	    {context, Binary4, [{Text, {Link, Title}} | Context]};
-	<<_:1/binary,Binary3/binary>> ->
+	<<_:1/binary, _Binary3/binary>> ->
 	    {syntax_error, expected_paren_bracket_or_colon}
     end.
 
 %% @doc parse the text portion of a link.
 %%      For example, parse "test" from [test][this].
-parse_link_text(<<"\n",Binary/binary>>, Acc) ->
+parse_link_text(<<"\n", _Binary/binary>>, _Acc) ->
     {syntax_error, unexpected_newline_in_link};
 parse_link_text(<<"[",Binary/binary>>, Acc) ->
     parse_link_text(Binary, Acc);
@@ -132,17 +132,19 @@ parse_link_text(<<Char:1/binary, Binary/binary>>, Acc) ->
 parse_link_remainder(<<Binary/binary>>, <<EndChar:1/binary>>) ->
     parse_link_remainder(Binary, EndChar, [], [], link).
 parse_link_remainder(<<EndChar:1/binary>>, EndChar, LinkAcc, TitleAcc, _) ->
-    Link = lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(LinkAcc))),
-    Title = lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(TitleAcc))),
+    Link = list_to_binary(lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(LinkAcc)))),
+    Title = list_to_binary(lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(TitleAcc)))),
     {<<"">>, Link, Title};
 parse_link_remainder(<<EndChar:1/binary, Binary/binary>>, EndChar, LinkAcc, TitleAcc, _) ->
-    Link = lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(LinkAcc))),
-    Title = lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(TitleAcc))),
+    Link = list_to_binary(lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(LinkAcc)))),
+    Title = list_to_binary(lists:append(lists:map(fun(X) -> binary_to_list(X) end, lists:reverse(TitleAcc)))),
     {Binary, Link, Title};
 parse_link_remainder(<<"\"", Binary/binary>>, EndChar, LinkAcc, TitleAcc, title) ->
     parse_link_remainder(<<Binary/binary>>, EndChar, LinkAcc, TitleAcc, done);
 parse_link_remainder(<<"\"", Binary/binary>>, EndChar, LinkAcc, [], link) ->
     parse_link_remainder(Binary, EndChar, LinkAcc, [], title);
+parse_link_remainder(<<" ", Binary/binary>>, EndChar, LinkAcc, [], link) ->
+    parse_link_remainder(Binary, EndChar, LinkAcc, [], link);
 parse_link_remainder(<<Char:1/binary, Binary/binary>>, EndChar, LinkAcc, [], link) ->
     parse_link_remainder(Binary, EndChar, [Char | LinkAcc], [], link);
 parse_link_remainder(<<"\"", Binary/binary>>, EndChar, LinkAcc, TitleAcc, title) ->
